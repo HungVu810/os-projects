72e7bdd9921c1af9b22f4b643473fcaa029bf123
HungVu810 <63895487+HungVu810@users.noreply.github.com>
2023-05-08 20:48:01 -0700

    Finished the simple implementation, now working on demand paging

--- project2/include/MemoryManager.h
+++ project2/include/MemoryManager.h
@@ -4,15 +4,7 @@
 #include <string>
 #include <array>
 #include <vector>
-
-//uint32_t address // user input
-//std::numeric_limit<uint32_t>()
-//
-//[s, p, w] = char* addressBytes[sizeof(uint32_t)] divided among s(sz (7), frame# to p (7) -> p (9) -> w within page (9)
-//
-//
-//      s       p      w
-//    9 bit   9 bit   9 bit
+#include <optional>
 
 class MemoryManager
 {
@@ -22,11 +14,10 @@ public:
 		, disk(512, std::vector<uint32_t>(1024))
 	{}
 
-    void init(std::filesystem::path initFile, std::filesystem::path vaFile)
+    void init(std::filesystem::path initFilePath)
     {
-		auto inputFile = std::ifstream{initFile};
+		auto inputFile = std::ifstream{initFilePath};
 		if (!inputFile) throw std::runtime_error{"Invalid input file."};
-		auto outputFile = std::ofstream{initFile.parent_path()/"output.txt"}; // Create an output file, std::fstream{path, std::ios::out};
 		auto segmentCommand = std::string{};
 		auto pageCommand = std::string{};
 		if (!std::getline(inputFile, segmentCommand)) throw std::runtime_error{"Failed to get input command to initialize a segment table."};
@@ -36,6 +27,24 @@ public:
 		initPhysicalMemory(segmentInfos, pageInfos);
     }
 
+	void parseVirtualAddresses(std::filesystem::path vaFilePath)
+	{
+		auto vaFile = std::ifstream{vaFilePath};
+		if (!vaFile) throw std::runtime_error{"Invalid input file."};
+		auto outputFile = std::ofstream{vaFilePath.parent_path()/"output.txt"}; // Create an output file
+		auto command = std::string{};
+		while (std::getline(vaFile, command))
+		{
+			const auto vaStrings = tokenizeCommand(command);
+			for (const auto& vaString : vaStrings)
+			{
+				const auto vaTranslateInfo = translateVirtualAddress(std::stoul(vaString));
+				const auto pa = getPhysicalAddress(vaTranslateInfo);
+				if (pa.has_value()) outputFile << pa.value() << " ";
+				else outputFile << -1;
+			}
+		}
+	}
 
 private:
 	struct SegmentInfo // A segment at 'frame' owns multiples pages. The pages are resided at different frame and may/may not be contiguous to one another
@@ -46,46 +55,59 @@ private:
 	};
 	struct PageInfo
 	{
-		uint32_t number;
 		uint32_t segment; // Owner
+		uint32_t number;
 		uint32_t frame; // Residing location
 	};
 	struct TranslateInfo
 	{
 		uint32_t s;
-		uint32_t p;
 		uint32_t w;
+		uint32_t p;
 		uint32_t pw;
+		uint32_t va; // Original virtual address
 	};
 
 	void initPhysicalMemory(const std::vector<SegmentInfo>& segmentInfos, const std::vector<PageInfo>& pageInfos)
 	{
 		for (const auto& segmentInfo : segmentInfos)
 		{
-			physicalMemory[2ull * segmentInfo.number] = segmentInfo.size;
-			physicalMemory[2ull * segmentInfo.number + 1ull] = segmentInfo.frame;
+			physicalMemory[2ull * segmentInfo.number] = segmentInfo.size; // PM[2s] = segmentSize
+			physicalMemory[2ull * segmentInfo.number + 1ull] = segmentInfo.frame; // PM[2s + 1] = segmentFrame
 		}
 
 		for (const auto& pageInfo : pageInfos)
 		{
-			physicalMemory[(2ull * pageInfo.segment + 1ull) * 512ull + pageInfo.number] = pageInfo.frame;
+			physicalMemory[physicalMemory[2ull * pageInfo.segment + 1ull] * 512ull + pageInfo.number] = pageInfo.frame; // PM[PM[2s + 1] * 512 + p] = w
 		}
 	}
 
-	void interpretVirtualAddress(const TranslateInfo& va)
+	std::optional<uint32_t> getPhysicalAddress(const TranslateInfo& va)
 	{
+		if (va.pw >= physicalMemory[2ull * va.s]) return std::nullopt;
+		return static_cast<uint32_t>(physicalMemory[physicalMemory[2ull * va.s + 1ull] * 512ull + va.p] * 512ull + va.w);
+
 		// PA = PM[PM[2s+1]*512+p]*512+w, check for page fault
 		// s: 9 bit, p: 9 bit, w:; 9 bit, present bit: 1 bit
 		// The sign bit is used as the present bit (negative = not resident
+
+		//uint32_t address // user input
+		//std::numeric_limit<uint32_t>()
+		//
+		//[s, p, w] = char* addressBytes[sizeof(uint32_t)] divided among s(sz (7), frame# to p (7) -> p (9) -> w within page (9)
+		//
+		//
 	}
 
 	TranslateInfo translateVirtualAddress(uint32_t va)
 	{
+		// 32 bits = (5 empty bits) (9 bits = s) (9 bits = p) (9 bits = w)
 		return TranslateInfo{
-			va >> 18
-			, va & 0x1FF
-			, (va >> 9) & 0x1FF
-			, va & 0x3FFF
+			va >> 18 // s
+			, va & 0x1FF // w
+			, (va >> 9) & 0x1FF // p
+			, va & 0x3FFF // pw
+			, va // va
 		};
 	}
 
--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, const char *const *const argv)
 	auto arguments = std::vector<std::string_view>(argv, argv + argc);
 
 	auto memoryManager = MemoryManager{};
-	if (arguments.size() != 2) throw std::runtime_error{"Missing input file name"};
+	if (arguments.size() != 3) throw std::runtime_error{"Missing input file name"};
 	memoryManager.init(arguments[1]);
+	memoryManager.parseVirtualAddresses(arguments[2]);
 }
